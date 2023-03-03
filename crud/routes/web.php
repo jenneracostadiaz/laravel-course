87942af6286395ca3eaa462b12806a6ff6550394
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-03-03 16:07:42 -0500

    crud

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/note', [NoteController::class, 'index'])->name('note.index');
 Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
 Route::post('/note/store', [NoteController::class, 'store'])->name('note.store');
@@ -11,3 +10,5 @@ Route::get('/note/edit/{note}', [NoteController::class, 'edit'])->name('note.edi
 Route::put('/note/update/{note}', [NoteController::class, 'update'])->name('note.update');
 Route::get('/note/show/{note}', [NoteController::class, 'show'])->name('note.show');
 Route::delete('/note/destroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+Route::resource('/post', PostController::class);
