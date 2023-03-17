84582db83953510fa97eb83139b00103a53a9b74
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-03-17 15:52:10 -0500

    Relation One to One

---
+++ relationships/database/seeders/PhoneSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Phone;
+use Illuminate\Database\Seeder;
+
+class PhoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Phone::create([
+            'prefix' => 51,
+            'phone_number' => 984651354,
+            'user_id' => 1
+        ]);
+    }
+}
---
+++ relationships/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'id' => 1,
+            'name' => 'example',
+            'email' => 'example@example.com',
+            'password' => Hash::make(123456789)
+        ]);
+    }
+}
