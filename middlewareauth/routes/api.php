69b4a525d012e6fbfda6e2579efb362a9b9617ca
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-05-01 07:16:55 -0500

    API Auth Sanctum

--- middlewareauth/routes/api.php
+++ middlewareauth/routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::post('/create', [AuthController::class, 'createUser']);
+Route::post('/login', [AuthController::class, 'loginUser']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/no-access', [ExampleController::class, 'noAccess'])->name('no-access');
-Route::get('/', [ExampleController::class, 'index']);
