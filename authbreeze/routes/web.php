096e46e4ef60ffcae4c3ddcf0a9da617b4c94399
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-05-09 11:19:36 -0500

    Auth Breeze

--- authbreeze/routes/web.php
+++ authbreeze/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/example', [ExampleController::class, 'index'])->name('example');
+});
+
+require __DIR__ . '/auth.php';
