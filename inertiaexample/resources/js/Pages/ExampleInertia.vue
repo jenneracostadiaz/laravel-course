5703ff88ae7dda361a560a1f2f801095ece40bbd
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-05-10 11:17:00 -0500

    inertia

---
+++ inertiaexample/resources/js/Pages/ExampleInertia.vue
@@ -0,0 +1,30 @@
+<template>
+	<h1>{{ counter }}</h1>
+	<p>
+		<button @click="increment()">+ Increment</button>
+	</p>
+	<p>
+		<button @click="minus()">- Minus</button>
+	</p>
+	<p>
+		<button @click="restorecounter()">Restore</button>
+	</p>
+	<NavLink :href="route('login')">Login</NavLink>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import NavLink from '@/Components/NavLink.vue';
+const counter = ref(0);
+const increment = () => {
+	counter.value++;
+};
+const minus = () => {
+	counter.value--;
+};
+const restorecounter = () => {
+	counter.value = 0;
+};
+</script>
+
+<style scoped></style>
---
+++ inertiaexample/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ExampleController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/example', [ExampleController::class, 'index'])->name('example');
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
